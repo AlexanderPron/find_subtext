6e0b6f8fa0fa616d4555cba1574c68ddbe76b85f
Gadya <zakuson2001@inbox.ru>
2023-02-16 01:33:21 +0300

    first for Harry. Doesn	 work

---
+++ find_subtext.py
@@ -0,0 +1,99 @@
+from orderedset import OrderedSet
+
+
+russianAlphabet = {
+    'й', 'ф', 'я', 'ц', 'ы', 'ч', 'у', 'в', 'с', 'к', 'а',
+    'м', 'е', 'п', 'и', 'н', 'р', 'т', 'г', 'о', 'ь', 'ш',
+    'л', 'б', 'щ', 'д', 'ю', 'з', 'ж', 'х', 'э', 'ъ', 'ё'
+}
+
+
+def compareTwoTexts(txt1, txt2, alphabet=russianAlphabet):
+    # txt1 should be the shorter one
+    ngramd1 = extractNGrams(txt1, alphabet)
+    ngramd2 = extractNGrams(txt2, alphabet)
+    return extractCommonPassages(getCommonNGrams(ngramd1, ngramd2))
+
+
+def extractNGrams(txt, alphabet):
+    words = extractWords(txt, alphabet)
+    ngrams = []
+    for i in range(len(words) - 1):
+        ngrams.append(
+            (words[i] + ' ' + words[i + 1], i)
+        )
+    ngramDic = {}
+    for ngram in ngrams:
+        try:
+            ngramDic[ngram[0]].append(ngram[1])
+        except KeyError:
+            ngramDic[ngram[0]] = [ngram[1]]
+    return ngramDic
+
+
+def extractWords(s, alphabet):
+    arr = []
+    temp = []
+    for char in s.lower():
+        if char in alphabet or char == '-' and len(temp) > 0:
+            temp.append(char)
+        else:
+            if len(temp) > 0:
+                arr.append(''.join(temp))
+                temp = []
+    if len(temp) > 0:
+        arr.append(''.join(temp))
+    return arr
+
+
+def getReverseDic(ngramDic):
+    reverseDic = {}
+    for key in ngramDic:
+        for index in ngramDic[key]:
+            reverseDic[index] = key
+    return reverseDic
+
+
+def getCommonNGrams(ngramDic1, ngramDic2):
+    # ngramDic1 should be for the shorter text
+    allCommonNGrams = []
+    for nGram in ngramDic1:
+        if nGram in ngramDic2:
+            for i in ngramDic1[nGram]:
+                for j in ngramDic2[nGram]:
+                    allCommonNGrams.append((nGram, i, j))
+    allCommonNGrams.sort(key=lambda x: x[1])
+    commonNGramSet = OrderedSet((item[1], item[2]) for item in allCommonNGrams)
+    return commonNGramSet
+
+
+def extractCommonPassages(commonNGrams):
+    if not commonNGrams:
+        raise ValueError('no common ngrams')
+    commonPassages = []
+    temp = []
+    while commonNGrams:
+        if not temp:
+            temp = [commonNGrams.popFirst()]
+            if not commonNGrams:
+                break
+        if (temp[-1][0] + 1, temp[-1][1] + 1) in commonNGrams:
+            temp.append((temp[-1][0] + 1, temp[-1][1] + 1))
+            commonNGrams.discard((temp[-1][0], temp[-1][1]))
+        else:
+            commonPassages.append(temp)
+            temp = []
+    if temp:
+        commonPassages.append(temp)
+    return commonPassages
+
+
+def main():
+    txt1 = 'абра кадабра'
+    txt2 = 'абра кадабра швабар'
+
+    print(compareTwoTexts(txt1, txt2))
+
+
+if __name__ == '__main__':
+    main()
